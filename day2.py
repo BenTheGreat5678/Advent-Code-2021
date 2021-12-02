34b1482cdf12d1139ad22f985a8cf215e93a0175
Ben Holden <benjamin.p.holden@gmail.com>
2021-12-02 14:45:41 -0700

    Day 2 Complete

---
+++ day2.py
@@ -0,0 +1,20 @@
+with open("advent.txt") as nums:
+    num_list = [num.replace("\n", "") for num in nums.readlines()]
+
+position = 0
+depth = 0
+aim = 0
+
+for dir in num_list:
+    dir_num = dir.split()
+    if dir_num[0] == "forward":
+        position += int(dir_num[1])
+        depth += aim * int(dir_num[1])
+    elif dir_num[0] == "down":
+        aim += int(dir_num[1])
+    else:
+        aim -= int(dir_num[1])
+
+print(position * depth)
+
+# 1960569556
